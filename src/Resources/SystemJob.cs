8dfaa8e50f48e36258c1116bca63b1c3c5e1d806
teramako <teramako@gmail.com>
2024-06-17 18:34:07 +0900

    mv `AWX.Resources.IJobDetail` interface to `IJobDetail.cs`

--- src/Resources/SystemJob.cs
+++ src/Resources/SystemJob.cs
@@ -21,19 +21,6 @@ namespace AWX.Resources
         string ResultStdout { get; }
 
     }
-    public interface IJobDetail
-    {
-        [JsonPropertyName("job_args")]
-        string JobArgs { get; }
-        [JsonPropertyName("job_cwd")]
-        string JobCwd { get; }
-        [JsonPropertyName("job_env")]
-        Dictionary<string, string> JobEnv { get; }
-        [JsonPropertyName("result_traceback")]
-        string ResultTraceback { get; }
-        [JsonPropertyName("event_processing_finished")]
-        bool EventProcessingFinished { get; }
-    }
 
     public class SystemJob(ulong id, ResourceType type, string url, RelatedDictionary related,
                            SystemJob.Summary summaryFields, DateTime created, DateTime? modified, string name,
