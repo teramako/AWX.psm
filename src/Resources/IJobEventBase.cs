b4230fbb44ccababdfa1995ca32eecada11419d9
teramako <teramako@gmail.com>
2024-06-20 20:57:31 +0900

    Implement: `IJobEventBase` interface

---
+++ src/Resources/IJobEventBase.cs
@@ -0,0 +1,26 @@
+using System.Collections.Specialized;
+using System.Text.Json.Serialization;
+
+namespace AWX.Resources
+{
+    public interface IJobEventBase
+    {
+        DateTime Created { get; }
+        DateTime? Modified { get; }
+        JobEventEvent Event { get; }
+        int Counter { get; }
+        [JsonPropertyName("event_display")]
+        string EventDisplay { get; }
+        [JsonPropertyName("event_data")]
+        OrderedDictionary EventData { get; }
+        bool Failed { get; }
+        bool Changed { get; }
+        string UUID { get; }
+        string Stdout { get; }
+        [JsonPropertyName("start_line")]
+        int StartLine { get; }
+        [JsonPropertyName("end_line")]
+        int EndLine { get; }
+        JobVerbosity Verbosity { get; }
+    }
+}
