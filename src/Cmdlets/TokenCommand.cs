9bcb6d2871efe23264bd0fe72c37ae56a6897047
teramako <teramako@gmail.com>
2024-06-26 18:53:44 +0900

    [psm] Implement cmdlet for getting/finding Token
    - `Get-Token`
    - `Find-Token`

---
+++ src/Cmdlets/TokenCommand.cs
@@ -0,0 +1,83 @@
+using AWX.Resources;
+using System.Management.Automation;
+
+namespace AWX.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "Token")]
+    [OutputType(typeof(Application))]
+    public class GetTokenCommand : GetCmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            foreach (var id in Id)
+            {
+                IdSet.Add(id);
+            }
+        }
+        protected override void EndProcessing()
+        {
+            Query.Add("id__in", string.Join(',', IdSet));
+            Query.Add("page_size", $"{IdSet.Count}");
+            foreach(var token in OAuth2AccessToken.Find(Query).ToBlockingEnumerable())
+            {
+                WriteObject(token);
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "Token", DefaultParameterSetName = "All")]
+    [OutputType(typeof(Application))]
+    public class FindTokenCommand : FindCmdletBase
+    {
+        [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        public override ulong Id { get; set; }
+        [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        [ValidateSet(nameof(ResourceType.OAuth2Application),
+                     nameof(ResourceType.User))]
+        public override ResourceType Type { get; set; }
+
+        /// <summary>
+        /// Filter by Personal Access Token(<c>Personal</c>) or
+        /// User Authorized Token(<c>Authorized</c>) or both.<br/>
+        /// Ignored when <c>Type</c> parameter is <c>OAuth2Application</c>
+        /// </summary>
+        [Parameter()]
+        public ETokenType TokenType { get; set; } = ETokenType.Both;
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["id"];
+
+        public enum ETokenType
+        {
+            Both, Personal, Authorized
+        }
+
+        protected override void ProcessRecord()
+        {
+            Query.Clear();
+            if (Type != ResourceType.OAuth2Application)
+            {
+                switch (TokenType)
+                {
+                    case ETokenType.Personal:
+                        Query.Add("application", "null");
+                        break;
+                    case ETokenType.Authorized:
+                        Query.Add("not__application", "null");
+                        break;
+                }
+            }
+            SetupCommonQuery();
+            IAsyncEnumerable<OAuth2AccessToken> asyncTokens = Type switch
+            {
+                ResourceType.OAuth2Application => OAuth2AccessToken.FindFromApplication(Id, Query, All),
+                ResourceType.User => OAuth2AccessToken.FindFromUser(Id, Query, All),
+                _ => OAuth2AccessToken.Find(Query, All),
+            };
+            foreach (var token in asyncTokens.ToBlockingEnumerable())
+            {
+                WriteObject(token);
+            }
+        }
+    }
+}
