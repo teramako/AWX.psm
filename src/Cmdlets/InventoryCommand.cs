2dbd02f30aad2e9525ab04197c8a8f80092670df
teramako <teramako@gmail.com>
2024-07-02 16:40:36 +0900

    [psm] Implement cmdlet for getting/finding Inventories
    - `Get-Inventory`
    - `Find-Inventory`

---
+++ src/Cmdlets/InventoryCommand.cs
@@ -0,0 +1,94 @@
+using AWX.Resources;
+using System.Management.Automation;
+
+namespace AWX.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "Inventory")]
+    [OutputType(typeof(Inventory))]
+    public class GetInventoryCommand : GetCmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            foreach (var id in Id)
+            {
+                IdSet.Add(id);
+            }
+        }
+        protected override void EndProcessing()
+        {
+            string path;
+            if (IdSet.Count == 1)
+            {
+                path = $"{Inventory.PATH}{IdSet.First()}/";
+                var res = GetResource<Inventory>(path);
+                WriteObject(res);
+            }
+            else
+            {
+                path = Inventory.PATH;
+                Query.Add("id__in", string.Join(',', IdSet));
+                Query.Add("page_size", $"{IdSet.Count}");
+                foreach (var resultSet in GetResultSet<Inventory>(path, Query))
+                {
+                    WriteObject(resultSet.Results, true);
+                }
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "Inventory", DefaultParameterSetName = "All")]
+    [OutputType(typeof(Inventory))]
+    public class FindInventoryCommand : FindCmdletBase
+    {
+        [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        [ValidateSet(nameof(ResourceType.Organization),
+                     nameof(ResourceType.Inventory),
+                     nameof(ResourceType.Host))]
+        public override ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public InventoryKind Kind { get; set; } = InventoryKind.All;
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["id"];
+
+        public enum InventoryKind
+        {
+            All, Normal, Smart, Constructed
+        }
+
+        protected override void BeginProcessing()
+        {
+            switch (Kind)
+            {
+                case InventoryKind.Normal:
+                    Query.Add("kind", "");
+                    break;
+                case InventoryKind.Smart:
+                case InventoryKind.Constructed:
+                    Query.Add("kind", Kind.ToString().ToLowerInvariant());
+                    break;
+                case InventoryKind.All:
+                default:
+                    break;
+            }
+            SetupCommonQuery();
+        }
+        protected override void ProcessRecord()
+        {
+            var path = Type switch
+            {
+                ResourceType.Organization => $"{Organization.PATH}{Id}/inventories/",
+                ResourceType.Inventory => $"{Inventory.PATH}{Id}/input_inventories/",
+                ResourceType.Host => $"{Host.PATH}{Id}/smart_inventories/",
+                _ => Inventory.PATH
+            };
+            foreach (var resultSet in GetResultSet<Inventory>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+}
