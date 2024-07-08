237f47e1701cf943bd0c17de66aa6f68be1e33a5
teramako <teramako@gmail.com>
2024-07-08 15:10:46 +0900

    [psm] Implement cmdlet: gettting/finding JobHostSummary
    - `Get-JobHostSummary`
    - `Find-JobHostSummary`

---
+++ src/Cmdlets/JobHostSummaryCommand.cs
@@ -0,0 +1,58 @@
+using AWX.Resources;
+using System.Management.Automation;
+
+namespace AWX.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "JobHostSummary")]
+    [OutputType(typeof(JobHostSummary))]
+    public class GetJobHostSummaryCommand : GetCmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            foreach (var id in Id)
+            {
+                if (!IdSet.Add(id))
+                {
+                    // skip already processed
+                    continue;
+                }
+                var res = GetResource<JobHostSummary>($"{JobHostSummary.PATH}{id}/");
+                if (res != null)
+                {
+                    WriteObject(res);
+                }
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "JobHostSummary")]
+    [OutputType(typeof(JobHostSummary))]
+    public class FindJobHostSummaryCommand : FindCmdletBase
+    {
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, Position = 0)]
+        [ValidateSet(nameof(ResourceType.Job),
+                     nameof(ResourceType.Host),
+                     nameof(ResourceType.Group))]
+        public override ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, Position = 1)]
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["!id"];
+
+        protected override void EndProcessing()
+        {
+            var path = Type switch
+            {
+                ResourceType.Job => $"{JobTemplateJob.PATH}{Id}/job_host_summaries/",
+                ResourceType.Host => $"{Host.PATH}{Id}/job_host_summaries/",
+                ResourceType.Group => $"{Group.PATH}{Id}/job_host_summaries/",
+                _ => throw new ArgumentException()
+            };
+            foreach (var resultSet in GetResultSet<JobHostSummary>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+}
