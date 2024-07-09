4dfc1c8c724d6d5eb149c64d2201ea421ee0b536
teramako <teramako@gmail.com>
2024-07-09 11:42:05 +0900

    [psm] Implement cmdlet: gettting/finding SystemJobTemplate

---
+++ src/Cmdlets/SystemJobTemplateCommand.cs
@@ -0,0 +1,53 @@
+using AWX.Resources;
+using System.Management.Automation;
+
+namespace AWX.Cmdlets
+{
+
+    [Cmdlet(VerbsCommon.Get, "SystemJobTemplate")]
+    [OutputType(typeof(SystemJobTemplate))]
+    public class GetSystemJobTemplateCommand : GetCmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            foreach (var id in Id)
+            {
+                IdSet.Add(id);
+            }
+        }
+        protected override void EndProcessing()
+        {
+            Query.Add("id__in", string.Join(',', IdSet));
+            Query.Add("page_size", $"{IdSet.Count}");
+            foreach (var resultSet in GetResultSet<SystemJobTemplate>($"{SystemJobTemplate.PATH}?{Query}", true))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "SystemJobTemplate")]
+    [OutputType(typeof(SystemJobTemplate))]
+    public class FindSystemJobTemplateCommand : FindCmdletBase
+    {
+        public override ResourceType Type { get; set; }
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["id"];
+
+
+        protected override void BeginProcessing()
+        {
+            SetupCommonQuery();
+        }
+        protected override void ProcessRecord()
+        {
+            var path = SystemJobTemplate.PATH;
+            foreach (var resultSet in GetResultSet<SystemJobTemplate>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+}
