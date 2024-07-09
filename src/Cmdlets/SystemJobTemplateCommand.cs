da6a0944cfcc2a81c5b34e99f4d4e711f39491dd
teramako <teramako@gmail.com>
2024-07-09 13:20:00 +0900

    [psm] Implement cmdlet: `Invoke-SystemJobTemplate`

--- src/Cmdlets/SystemJobTemplateCommand.cs
+++ src/Cmdlets/SystemJobTemplateCommand.cs
@@ -1,4 +1,5 @@
 using AWX.Resources;
+using System.Collections;
 using System.Management.Automation;
 
 namespace AWX.Cmdlets
@@ -48,6 +49,77 @@ namespace AWX.Cmdlets
             {
                 WriteObject(resultSet.Results, true);
             }
+        }
+    }
+
+    [Cmdlet(VerbsLifecycle.Invoke, "SystemJobTemplate")]
+    [OutputType(typeof(SystemJob))]
+    public class InvokeSystemJobTemplateCommand : InvokeJobBase
+    {
+        [Parameter(Mandatory = true, ParameterSetName = "Id", ValueFromPipeline = true, Position = 0)]
+        [Parameter(Mandatory = true, ParameterSetName = "AsyncId", ValueFromPipeline = true, Position = 0)]
+        public ulong Id { get; set; }
+        [Parameter(Mandatory = true, ParameterSetName = "Template", ValueFromPipeline = true, Position = 0)]
+        [Parameter(Mandatory = true, ParameterSetName = "AsyncTemplate", ValueFromPipeline = true, Position = 0)]
+        public SystemJobTemplate? SystemJobTemplate { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = "AsyncId")]
+        [Parameter(Mandatory = true, ParameterSetName = "AsyncTemplate")]
+        public SwitchParameter Async { get; set; }
+
+        [Parameter()]
+        public IDictionary? ExtraVars { get; set; }
+
+        [Parameter(ParameterSetName = "Id")]
+        [Parameter(ParameterSetName = "Template")]
+        [ValidateRange(5, int.MaxValue)]
+        public int IntervalSeconds { get; set; } = 5;
+
+        [Parameter(ParameterSetName = "Id")]
+        [Parameter(ParameterSetName = "Template")]
+        public SwitchParameter SuppressJobLog { get; set; }
+
+        private Hashtable CreateSendData()
+        {
+            var dict = new Hashtable();
+            if (ExtraVars != null)
+            {
+                dict.Add("extra_vars", ExtraVars);
+            }
+            return dict;
+        }
+        private void Launch(ulong id)
+        {
+            var launchResult = CreateResource<SystemJob>($"{SystemJobTemplate.PATH}{id}/launch/", CreateSendData());
+            if (launchResult == null)
+            {
+                return;
+            }
+            WriteVerbose($"Launch SystemJobTemplate:{id} => Job:[{launchResult.Id}]");
+            if (Async)
+            {
+                WriteObject(launchResult, false);
+            }
+            else
+            {
+                jobTasks.Add(launchResult.Id, new JobTask(launchResult));
+            }
+        }
+        protected override void ProcessRecord()
+        {
+            if (SystemJobTemplate != null)
+            {
+                Id = SystemJobTemplate.Id;
+            }
+            Launch(Id);
+        }
+        protected override void EndProcessing()
+        {
+            if (Async)
+            {
+                return;
+            }
+            WaitJobs("Launch SystemJobTemplate", IntervalSeconds, SuppressJobLog);
         }
     }
 }
