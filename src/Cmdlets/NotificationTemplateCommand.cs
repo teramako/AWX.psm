4a2e67232637cc534ae565126e2fac04bc3bd059
teramako <teramako@gmail.com>
2024-07-10 15:21:26 +0900

    [psm] Implement cmdlet: gettting/finding NotificationTemplate
    - `Get-NotificationTemplate`
    - `Find-NotificationTemplate`

---
+++ src/Cmdlets/NotificationTemplateCommand.cs
@@ -0,0 +1,69 @@
+using AWX.Resources;
+using System.Management.Automation;
+
+namespace AWX.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "NotificationTemplate")]
+    [OutputType(typeof(NotificationTemplate))]
+    public class GetNotificationTemplateCommand : GetCmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            foreach (var id in Id)
+            {
+                IdSet.Add(id);
+            }
+        }
+        protected override void EndProcessing()
+        {
+            string path;
+            if (IdSet.Count == 1)
+            {
+                path = $"{NotificationTemplate.PATH}{IdSet.First()}/";
+                var res = GetResource<NotificationTemplate>(path);
+                WriteObject(res);
+            }
+            else
+            {
+                path = NotificationTemplate.PATH;
+                Query.Add("id__in", string.Join(',', IdSet));
+                Query.Add("page_size", $"{IdSet.Count}");
+                foreach (var resultSet in GetResultSet<NotificationTemplate>(path, Query))
+                {
+                    WriteObject(resultSet.Results, true);
+                }
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "NotificationTemplate", DefaultParameterSetName = "All")]
+    [OutputType(typeof(NotificationTemplate))]
+    public class FindNotificationTemplateCommand : FindCmdletBase
+    {
+        [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        [ValidateSet(nameof(ResourceType.Organization))]
+        public override ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["id"];
+
+        protected override void BeginProcessing()
+        {
+            SetupCommonQuery();
+        }
+        protected override void ProcessRecord()
+        {
+            var path = Type switch
+            {
+                ResourceType.Organization => $"{Organization.PATH}{Id}/notification_templates/",
+                _ => NotificationTemplate.PATH
+            };
+            foreach (var resultSet in GetResultSet<NotificationTemplate>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+}
