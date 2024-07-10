31ff2d18aac47c834715368c72032cf5a3668f25
teramako <teramako@gmail.com>
2024-07-10 13:15:05 +0900

    [psm] Implement cmdlet: gettting/finding Role
    - `Get-Role`
    - `Find-Role`

---
+++ src/Cmdlets/RoleCommand.cs
@@ -0,0 +1,71 @@
+using AWX.Resources;
+using System.Management.Automation;
+
+namespace AWX.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "Role")]
+    [OutputType(typeof(Role))]
+    public class GetRoleCommand : GetCmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            foreach (var id in Id)
+            {
+                IdSet.Add(id);
+            }
+        }
+        protected override void EndProcessing()
+        {
+            string path;
+            if (IdSet.Count == 1)
+            {
+                path = $"{Role.PATH}{IdSet.First()}/";
+                var res = GetResource<Role>(path);
+                WriteObject(res);
+            }
+            else
+            {
+                path = Role.PATH;
+                Query.Add("id__in", string.Join(',', IdSet));
+                Query.Add("page_size", $"{IdSet.Count}");
+                foreach (var resultSet in GetResultSet<Role>(path, Query))
+                {
+                    WriteObject(resultSet.Results, true);
+                }
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "Role", DefaultParameterSetName = "All")]
+    [OutputType(typeof(Role))]
+    public class FindRoleCommand : FindCmdletBase
+    {
+        [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        [ValidateSet(nameof(ResourceType.User),
+                     nameof(ResourceType.Team))]
+        public override ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["id"];
+
+        protected override void BeginProcessing()
+        {
+            SetupCommonQuery();
+        }
+        protected override void ProcessRecord()
+        {
+            var path = Type switch
+            {
+                ResourceType.User => $"{User.PATH}{Id}/roles/",
+                ResourceType.Team => $"{Team.PATH}{Id}/roles/",
+               _ => Role.PATH
+            };
+            foreach (var resultSet in GetResultSet<Role>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+}
