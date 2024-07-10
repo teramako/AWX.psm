13708f2fb6fd7d64248e09778eec2cb29ec66311
teramako <teramako@gmail.com>
2024-07-10 15:26:25 +0900

    [psm] Implement cmdlet: Find-NotificationTemplateFor***
    - `Find-NotificationTemplateForApprovals`
    - `Find-NotificationTemplateForError`
    - `Find-NotificationTemplateForStarted`
    - `Find-NotificationTemplateForSuccess`

--- src/Cmdlets/NotificationTemplateCommand.cs
+++ src/Cmdlets/NotificationTemplateCommand.cs
@@ -66,4 +66,160 @@ namespace AWX.Cmdlets
             }
         }
     }
+
+    [Cmdlet(VerbsCommon.Find, "NotificationTemplateForApprovals")]
+    [OutputType(typeof(NotificationTemplate))]
+    public class FindNotificationTemplateForApprovalsCommand : FindCmdletBase
+    {
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        [ValidateSet(nameof(ResourceType.Organization),
+                     nameof(ResourceType.WorkflowJobTemplate))]
+        public override ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["id"];
+
+        protected override void BeginProcessing()
+        {
+            SetupCommonQuery();
+        }
+        protected override void ProcessRecord()
+        {
+            var path = Type switch
+            {
+                ResourceType.Organization => $"{Organization.PATH}{Id}/notification_templates_approvals/",
+                ResourceType.WorkflowJobTemplate => $"{WorkflowJobTemplate.PATH}{Id}/notification_templates_approvals/",
+                _ => throw new ArgumentException()
+            };
+            foreach (var resultSet in GetResultSet<NotificationTemplate>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "NotificationTemplateForError")]
+    [OutputType(typeof(NotificationTemplate))]
+    public class FindNotificationTemplateForErrorCommand : FindCmdletBase
+    {
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        [ValidateSet(nameof(ResourceType.Organization),
+                     nameof(ResourceType.Project),
+                     nameof(ResourceType.InventorySource),
+                     nameof(ResourceType.JobTemplate),
+                     nameof(ResourceType.SystemJobTemplate),
+                     nameof(ResourceType.WorkflowJobTemplate))]
+        public override ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["id"];
+
+        protected override void BeginProcessing()
+        {
+            SetupCommonQuery();
+        }
+        protected override void ProcessRecord()
+        {
+            var path = Type switch
+            {
+                ResourceType.Organization => $"{Organization.PATH}{Id}/notification_templates_error/",
+                ResourceType.Project => $"{Project.PATH}{Id}/notification_templates_error/",
+                ResourceType.InventorySource => $"{InventorySource.PATH}{Id}/notification_templates_error/",
+                ResourceType.JobTemplate => $"{JobTemplate.PATH}{Id}/notification_templates_error/",
+                ResourceType.SystemJobTemplate => $"{SystemJobTemplate.PATH}{Id}/notification_templates_error/",
+                ResourceType.WorkflowJobTemplate => $"{WorkflowJobTemplate.PATH}{Id}/notification_templates_error/",
+                _ => throw new ArgumentException()
+            };
+            foreach (var resultSet in GetResultSet<NotificationTemplate>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "NotificationTemplateForStarted")]
+    [OutputType(typeof(NotificationTemplate))]
+    public class FindNotificationTemplateForStartedCommand : FindCmdletBase
+    {
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        [ValidateSet(nameof(ResourceType.Organization),
+                     nameof(ResourceType.Project),
+                     nameof(ResourceType.InventorySource),
+                     nameof(ResourceType.JobTemplate),
+                     nameof(ResourceType.SystemJobTemplate),
+                     nameof(ResourceType.WorkflowJobTemplate))]
+        public override ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["id"];
+
+        protected override void BeginProcessing()
+        {
+            SetupCommonQuery();
+        }
+        protected override void ProcessRecord()
+        {
+            var path = Type switch
+            {
+                ResourceType.Organization => $"{Organization.PATH}{Id}/notification_templates_started/",
+                ResourceType.Project => $"{Project.PATH}{Id}/notification_templates_started/",
+                ResourceType.InventorySource => $"{InventorySource.PATH}{Id}/notification_templates_started/",
+                ResourceType.JobTemplate => $"{JobTemplate.PATH}{Id}/notification_templates_started/",
+                ResourceType.SystemJobTemplate => $"{SystemJobTemplate.PATH}{Id}/notification_templates_started/",
+                ResourceType.WorkflowJobTemplate => $"{WorkflowJobTemplate.PATH}{Id}/notification_templates_started/",
+                _ => throw new ArgumentException()
+            };
+            foreach (var resultSet in GetResultSet<NotificationTemplate>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "NotificationTemplateForSuccess")]
+    [OutputType(typeof(NotificationTemplate))]
+    public class FindNotificationTemplateForSuccessCommand : FindCmdletBase
+    {
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        [ValidateSet(nameof(ResourceType.Organization),
+                     nameof(ResourceType.Project),
+                     nameof(ResourceType.InventorySource),
+                     nameof(ResourceType.JobTemplate),
+                     nameof(ResourceType.SystemJobTemplate),
+                     nameof(ResourceType.WorkflowJobTemplate))]
+        public override ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["id"];
+
+        protected override void BeginProcessing()
+        {
+            SetupCommonQuery();
+        }
+        protected override void ProcessRecord()
+        {
+            var path = Type switch
+            {
+                ResourceType.Organization => $"{Organization.PATH}{Id}/notification_templates_success/",
+                ResourceType.Project => $"{Project.PATH}{Id}/notification_templates_success/",
+                ResourceType.InventorySource => $"{InventorySource.PATH}{Id}/notification_templates_success/",
+                ResourceType.JobTemplate => $"{JobTemplate.PATH}{Id}/notification_templates_success/",
+                ResourceType.SystemJobTemplate => $"{SystemJobTemplate.PATH}{Id}/notification_templates_success/",
+                ResourceType.WorkflowJobTemplate => $"{WorkflowJobTemplate.PATH}{Id}/notification_templates_success/",
+                _ => throw new ArgumentException()
+            };
+            foreach (var resultSet in GetResultSet<NotificationTemplate>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
 }
