80669c8897edb51b8931cd0270395a607824e1d6
teramako <teramako@gmail.com>
2024-07-11 15:29:16 +0900

    [psm] Implement cmdlet: gettting/finding WorkflowJobNode
    - `Get-WorkflowJobNode`
    - `Find-WorkflowJobNode`

---
+++ src/Cmdlets/WorkflowJobNodeCommand.cs
@@ -0,0 +1,58 @@
+using AWX.Resources;
+using System.Management.Automation;
+
+namespace AWX.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "WorkflowJobNode")]
+    [OutputType(typeof(WorkflowJobNode))]
+    public class GetWorkflowJobNodeCommand : GetCmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            foreach (var id in Id)
+            {
+                IdSet.Add(id);
+            }
+        }
+        protected override void EndProcessing()
+        {
+            Query.Add("id__in", string.Join(',', IdSet));
+            Query.Add("page_size", $"{IdSet.Count}");
+            foreach (var resultSet in GetResultSet<WorkflowJobNode>($"{WorkflowJobNode.PATH}?{Query}", true))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Find, "WorkflowJobNode", DefaultParameterSetName = "All")]
+    [OutputType(typeof(WorkflowJobNode))]
+    public class FindWorkflowJobNodeCommand : FindCmdletBase
+    {
+        [Parameter(ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        [ValidateSet(nameof(ResourceType.WorkflowJob))]
+        public override ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
+        public override ulong Id { get; set; }
+
+        [Parameter()]
+        public override string[] OrderBy { get; set; } = ["!id"];
+
+        protected override void BeginProcessing()
+        {
+            SetupCommonQuery();
+        }
+        protected override void ProcessRecord()
+        {
+            var path = Id switch
+            {
+                > 0 => $"{WorkflowJob.PATH}{Id}/workflow_nodes/",
+                _ => WorkflowJobNode.PATH
+            };
+            foreach (var resultSet in GetResultSet<WorkflowJobNode>(path, Query, All))
+            {
+                WriteObject(resultSet.Results, true);
+            }
+        }
+    }
+}
