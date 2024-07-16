dba26e42841d7894b4b1a3d5aead77fd7a525fed
teramako <teramako@gmail.com>
2024-07-16 11:43:53 +0900

    [psm] Implement cmdlet: `Invoke-WorkflowJobTemplate`

--- src/Cmdlets/WorkflowJobTemplateCommand.cs
+++ src/Cmdlets/WorkflowJobTemplateCommand.cs
@@ -1,4 +1,5 @@
 using AWX.Resources;
+using System.Collections;
 using System.Management.Automation;
 
 namespace AWX.Cmdlets
@@ -53,6 +54,108 @@ namespace AWX.Cmdlets
             {
                 WriteObject(resultSet.Results, true);
             }
+        }
+    }
+
+    [Cmdlet(VerbsLifecycle.Invoke, "WorkJobTemplate")]
+    [OutputType(typeof(JobTemplateJob), ParameterSetName = ["Id", "JobTemplate"])]
+    [OutputType(typeof(JobTemplateJob.LaunchResult), ParameterSetName = ["AsyncId", "AsyncJobTemplate"])]
+    [OutputType(typeof(JobTemplateLaunchRequirements), ParameterSetName = ["GetRequirementsId", "GetRequirementsJobTemplate"])]
+    public class InvokeWorkJobTemplateCommand : InvokeJobBase
+    {
+        [Parameter(Mandatory = true, ParameterSetName = "Id", ValueFromPipeline = true, Position = 0)]
+        [Parameter(Mandatory = true, ParameterSetName = "AsyncId", ValueFromPipeline = true, Position = 0)]
+        [Parameter(Mandatory = true, ParameterSetName = "GetRequirementsId", ValueFromPipeline = true, Position = 0)]
+        public ulong Id { get; set; }
+        [Parameter(Mandatory = true, ParameterSetName = "JobTemplate", ValueFromPipeline = true, Position = 0)]
+        [Parameter(Mandatory = true, ParameterSetName = "AsyncJobTemplate", ValueFromPipeline = true, Position = 0)]
+        [Parameter(Mandatory = true, ParameterSetName = "GetRequirementsJobTemplate", ValueFromPipeline = true, Position = 0)]
+        public WorkflowJobTemplate? WorkflowJobTemplate { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = "GetRequirementsId")]
+        [Parameter(Mandatory = true, ParameterSetName = "GetRequirementsJobTemplate")]
+        public SwitchParameter GetRequirements { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = "AsyncId")]
+        [Parameter(Mandatory = true, ParameterSetName = "AsyncJobTemplate")]
+        public SwitchParameter Async { get; set; }
+
+        [Parameter(ParameterSetName = "Id")]
+        [Parameter(ParameterSetName = "AsyncId")]
+        [Parameter(ParameterSetName = "JobTemplate")]
+        [Parameter(ParameterSetName = "AsyncJobTemplate")]
+        public string? Limit { get; set; }
+
+        [Parameter(ParameterSetName = "Id")]
+        [Parameter(ParameterSetName = "JobTemplate")]
+        [ValidateRange(5, int.MaxValue)]
+        public int IntervalSeconds { get; set; } = 5;
+
+        [Parameter(ParameterSetName = "Id")]
+        [Parameter(ParameterSetName = "JobTemplate")]
+        public SwitchParameter SuppressJobLog { get; set; }
+
+        private Hashtable CreateSendData()
+        {
+            var dict = new Hashtable();
+            if (Limit != null)
+            {
+                dict.Add("limit", Limit);
+            }
+            return dict;
+        }
+        private void GetLaunchRequirements(ulong id)
+        {
+            var res = base.GetResource<WorkflowJobTemplateLaunchRequirements>($"{WorkflowJobTemplate.PATH}{id}/launch/");
+            if (res == null)
+            {
+                return;
+            }
+            WriteObject(res, false);
+        }
+        private void Launch(ulong id)
+        {
+            var launchResult = CreateResource<WorkflowJob.LaunchResult>($"{WorkflowJobTemplate.PATH}{id}/launch/", CreateSendData());
+            if (launchResult == null)
+            {
+                return;
+            }
+            WriteVerbose($"Launch WorkflowJobTemplate:{id} => Job:[{launchResult.Id}]");
+            if (Async)
+            {
+                WriteObject(launchResult, false);
+            }
+            else
+            {
+                JobManager.Add(launchResult);
+            }
+        }
+        protected override void ProcessRecord()
+        {
+            if (WorkflowJobTemplate != null)
+            {
+                Id = WorkflowJobTemplate.Id;
+            }
+            if (GetRequirements)
+            {
+                GetLaunchRequirements(Id);
+            }
+            else
+            {
+                Launch(Id);
+            }
+        }
+        protected override void EndProcessing()
+        {
+            if (GetRequirements)
+            {
+                return;
+            }
+            if (Async)
+            {
+                return;
+            }
+            WaitJobs("Launch WorkflowJobTemplate", IntervalSeconds, SuppressJobLog);
         }
     }
 }
