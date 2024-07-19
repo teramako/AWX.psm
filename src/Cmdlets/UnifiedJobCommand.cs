c6676e9a903f8e86c23f094e2dfe8c045ad65e2b
teramako <teramako@gmail.com>
2024-07-19 15:57:18 +0900

    [psm] Implement cmdlet: `Stop-UnifiedJob`

--- src/Cmdlets/UnifiedJobCommand.cs
+++ src/Cmdlets/UnifiedJobCommand.cs
@@ -95,4 +95,61 @@ namespace AWX.Cmdlets
             WaitJobs("Wait Job", IntervalSeconds, SuppressJobLog);
         }
     }
+
+    [Cmdlet(VerbsLifecycle.Stop, "UnifiedJob", DefaultParameterSetName = "RequestCancel")]
+    [OutputType(typeof(PSObject))]
+    public class StopJobCommand : APICmdletBase
+    {
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, Position = 0)]
+        [ValidateSet(nameof(ResourceType.Job),
+                     nameof(ResourceType.ProjectUpdate),
+                     nameof(ResourceType.InventoryUpdate),
+                     nameof(ResourceType.SystemJob),
+                     nameof(ResourceType.AdHocCommand),
+                     nameof(ResourceType.WorkflowJob))]
+        public ResourceType Type { get; set; }
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, Position = 1)]
+        public ulong Id { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = "Determine")]
+        public SwitchParameter Determine { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var path = Type switch
+            {
+                ResourceType.Job => $"{JobTemplateJob.PATH}{Id}/cancel/",
+                ResourceType.ProjectUpdate => $"{ProjectUpdateJob.PATH}{Id}/cancel/",
+                ResourceType.InventoryUpdate => $"{InventoryUpdateJob.PATH}{Id}/cancel/",
+                ResourceType.AdHocCommand => $"{AdHocCommand.PATH}{Id}/cancel/",
+                ResourceType.SystemJob => $"{SystemJob.PATH}{Id}/cancel/",
+                ResourceType.WorkflowJob => $"{WorkflowJob.PATH}{Id}/cancel/",
+                _ => throw new NotImplementedException()
+            };
+            var psobject = new PSObject();
+            psobject.Members.Add(new PSNoteProperty("Id", Id));
+            psobject.Members.Add(new PSNoteProperty("Type", Type));
+            if (Determine)
+            {
+                var result = GetResource<Dictionary<string, bool>>(path);
+                if (result == null) return;
+                result.TryGetValue("can_cancel", out bool canCancel);
+                psobject.Members.Add(new PSNoteProperty("CanCancel", canCancel));
+                WriteObject(psobject);
+            }
+            else
+            {
+                try
+                {
+                    var apiResult = CreateResource<string>(path);
+                    psobject.Members.Add(new PSNoteProperty("Status", apiResult.Response.StatusCode));
+                }
+                catch (RestAPIException ex)
+                {
+                    psobject.Members.Add(new PSNoteProperty("Status", ex.Response.StatusCode));
+                }
+                WriteObject(psobject);
+            }
+        }
+    }
 }
