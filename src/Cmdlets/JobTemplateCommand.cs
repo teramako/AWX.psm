cea218704d73f289d2581b3a5595faf5538cc62d
teramako <teramako@gmail.com>
2024-07-21 01:20:46 +0900

    [psm] JobTemplate実行時に各種パラメータ情報を出力する
    `-GetRequirements` コマンドライン・パラメーターは廃止

--- src/Cmdlets/JobTemplateCommand.cs
+++ src/Cmdlets/JobTemplateCommand.cs
@@ -1,6 +1,7 @@
 using AWX.Resources;
 using System.Collections;
 using System.Management.Automation;
+using System.Text;
 
 namespace AWX.Cmdlets
 {
@@ -69,20 +70,13 @@ namespace AWX.Cmdlets
     public abstract class LaunchJobTemplateCommandBase : InvokeJobBase
     {
         [Parameter(Mandatory = true, ParameterSetName = "Id", ValueFromPipeline = true, Position = 0)]
-        [Parameter(Mandatory = true, ParameterSetName = "GetRequirementsId", ValueFromPipeline = true, Position = 0)]
         public ulong Id { get; set; }
         [Parameter(Mandatory = true, ParameterSetName = "JobTemplate", ValueFromPipeline = true, Position = 0)]
-        [Parameter(Mandatory = true, ParameterSetName = "GetRequirementsJobTemplate", ValueFromPipeline = true, Position = 0)]
         public JobTemplate? JobTemplate { get; set; }
-
-        [Parameter(Mandatory = true, ParameterSetName = "GetRequirementsId")]
-        [Parameter(Mandatory = true, ParameterSetName = "GetRequirementsJobTemplate")]
-        public SwitchParameter GetRequirements { get; set; }
 
         protected abstract bool Async { get; }
 
-        [Parameter(ParameterSetName = "Id")]
-        [Parameter(ParameterSetName = "JobTemplate")]
+        [Parameter()]
         public string? Limit { get; set; }
 
         private Hashtable CreateSendData()
@@ -94,17 +88,87 @@ namespace AWX.Cmdlets
             }
             return dict;
         }
-        protected void GetLaunchRequirements(ulong id)
+        private void ShowJobTemplateInfo(JobTemplateLaunchRequirements requirements)
         {
-            var res = GetResource<JobTemplateLaunchRequirements>($"{JobTemplate.PATH}{id}/launch/");
-            if (res == null)
+            var jt = requirements.JobTemplateData;
+            var def = requirements.Defaults;
+            WriteHost($"[{jt.Id}] {jt.Name} - {jt.Description}\n");
+            var fmt = "{0,22} : {1}\n";
+            if (def.Inventory.Id != null)
             {
-                return;
+                WriteHost(string.Format(fmt, "Inventory", $"[{def.Inventory.Id}] {def.Inventory.Name}"),
+                            foregroundColor: requirements.AskInventoryOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
             }
-            WriteObject(res, false);
+            if (!string.IsNullOrEmpty(def.Limit) || Limit != null)
+            {
+                var limitVal = def.Limit + (Limit != null ? $" => {Limit}" : "");
+
+                WriteHost(string.Format(fmt, "Limit", $"{limitVal}"),
+                            foregroundColor: requirements.AskLimitOnLaunch ? Limit == null ? ConsoleColor.Magenta : ConsoleColor.Green : null);
+            }
+            if (!string.IsNullOrEmpty(def.ScmBranch))
+            {
+                WriteHost(string.Format(fmt, "Scm Branch", def.ScmBranch),
+                            foregroundColor: requirements.AskScmBranchOnLaunch? ConsoleColor.Magenta : ConsoleColor.Green);
+            }
+            if (def.Labels != null && def.Labels.Length > 0)
+            {
+                WriteHost(string.Format(fmt, "Labels", string.Join(", ", def.Labels.Select(l => $"[{l.Id}] {l.Name}"))),
+                            foregroundColor: requirements.AskLabelsOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            }
+            if (!string.IsNullOrEmpty(def.JobTags))
+            {
+                WriteHost(string.Format(fmt, "Job tags", def.JobTags),
+                            foregroundColor: requirements.AskTagsOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            }
+            if (!string.IsNullOrEmpty(def.SkipTags))
+            {
+                WriteHost(string.Format(fmt, "Skip tags", def.SkipTags),
+                            foregroundColor: requirements.AskSkipTagsOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            }
+            if (!string.IsNullOrEmpty(def.ExtraVars))
+            {
+                var sb = new StringBuilder();
+                var lines = def.ExtraVars.Split('\n');
+                sb.Append(string.Format(fmt, "Extra vars", lines[0]));
+                foreach (var line in lines[1..])
+                {
+                    sb.AppendLine("".PadLeft(25) + line);
+                }
+                WriteHost(sb.ToString(),
+                            foregroundColor: requirements.AskVariablesOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            }
+            WriteHost(string.Format(fmt, "Diff Mode", def.DiffMode),
+                            foregroundColor: requirements.AskDiffModeOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            WriteHost(string.Format(fmt, "Job Type", def.JobType),
+                            foregroundColor: requirements.AskJobTypeOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            WriteHost(string.Format(fmt, "Verbosity", $"{def.Verbosity:d} ({def.Verbosity})"),
+                            foregroundColor: requirements.AskVerbosityOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            if (def.Credentials != null)
+            {
+                WriteHost(string.Format(fmt, "Credentials", string.Join(", ", def.Credentials.Select(c => $"[{c.Id}] {c.Name}"))),
+                            foregroundColor: requirements.AskCredentialOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            }
+            if (def.ExecutionEnvironment.Id != null)
+            {
+                WriteHost(string.Format(fmt, "ExecutionEnvironment", $"[{def.ExecutionEnvironment.Id}] {def.ExecutionEnvironment.Name}"),
+                            foregroundColor: requirements.AskExecutionEnvironmentOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            }
+            WriteHost(string.Format(fmt, "Forks", def.Forks),
+                            foregroundColor: requirements.AskForksOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            WriteHost(string.Format(fmt, "Job Slice Count", def.JobSliceCount),
+                            foregroundColor: requirements.AskJobSliceCountOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
+            WriteHost(string.Format(fmt, "Timeout", def.Timeout),
+                            foregroundColor: requirements.AskTimeoutOnLaunch ? ConsoleColor.Magenta : ConsoleColor.Green);
         }
-        protected JobTemplateJob.LaunchResult Launch(ulong id)
+        protected JobTemplateJob.LaunchResult? Launch(ulong id)
         {
+            var requirements = GetResource<JobTemplateLaunchRequirements>($"{JobTemplate.PATH}{id}/launch/");
+            if (requirements == null)
+            {
+                return null;
+            }
+            ShowJobTemplateInfo(requirements);
             var apiResult = CreateResource<JobTemplateJob.LaunchResult>($"{JobTemplate.PATH}{id}/launch/", CreateSendData());
             var launchResult = apiResult.Contents;
             WriteVerbose($"Launch JobTemplate:{id} => Job:[{launchResult.Id}]");
@@ -120,8 +184,7 @@ namespace AWX.Cmdlets
     }
 
     [Cmdlet(VerbsLifecycle.Invoke, "JobTemplate")]
-    [OutputType(typeof(JobTemplateJob), ParameterSetName = ["Id", "JobTemplate"])]
-    [OutputType(typeof(JobTemplateLaunchRequirements), ParameterSetName = ["GetRequirementsId", "GetRequirementsJobTemplate"])]
+    [OutputType(typeof(JobTemplateJob))]
     public class InvokeJobTemplateCommand : LaunchJobTemplateCommandBase
     {
         protected override bool Async { get; } = false;
@@ -141,34 +204,25 @@ namespace AWX.Cmdlets
             {
                 Id = JobTemplate.Id;
             }
-            if (GetRequirements)
+            try
             {
-                GetLaunchRequirements(Id);
-            }
-            else
-            {
-                try
+                var launchResult = Launch(Id);
+                if (launchResult != null)
                 {
-                    var launchResult = Launch(Id);
                     JobManager.Add(launchResult);
                 }
-                catch (RestAPIException) {}
             }
+            catch (RestAPIException) {}
         }
         protected override void EndProcessing()
         {
-            if (GetRequirements)
-            {
-                return;
-            }
             WaitJobs("Launch JobTemplate", IntervalSeconds, SuppressJobLog);
         }
 
     }
 
     [Cmdlet(VerbsLifecycle.Start, "JobTemplate")]
-    [OutputType(typeof(JobTemplateJob.LaunchResult), ParameterSetName = ["Id", "JobTemplate"])]
-    [OutputType(typeof(JobTemplateLaunchRequirements), ParameterSetName = ["GetRequirementsId", "GetRequirementsJobTemplate"])]
+    [OutputType(typeof(JobTemplateJob.LaunchResult))]
     public class StartJobTemplateCommand : LaunchJobTemplateCommandBase
     {
         protected override bool Async { get; } = true;
@@ -179,19 +233,15 @@ namespace AWX.Cmdlets
             {
                 Id = JobTemplate.Id;
             }
-            if (GetRequirements)
+            try
             {
-                GetLaunchRequirements(Id);
-            }
-            else
-            {
-                try
+                var launchResult = Launch(Id);
+                if (launchResult != null)
                 {
-                    var launchResult = Launch(Id);
                     WriteObject(launchResult, false);
                 }
-                catch (RestAPIException) { }
             }
+            catch (RestAPIException) {}
         }
     }
 }
