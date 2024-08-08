e7c7a34c825500219b599443190113ca4f7e9a5c
teramako <teramako@gmail.com>
2024-08-08 14:39:46 +0900

    Remove unncessary class: `ConfigAnalyticsCollectors`

--- src/Resources/Config.cs
+++ src/Resources/Config.cs
@@ -7,7 +7,7 @@ namespace AWX.Resources
                         string version,
                         string eula,
                         string configAnalyticsStatus,
-                        Config.ConfigAnalyticsCollectors analyticsCollectors,
+                        Dictionary<string, Config.ConfigValue> analyticsCollectors,
                         string[][] becomeMethods,
                         bool uiNext,
                         string projectBaseDir,
@@ -19,7 +19,7 @@ namespace AWX.Resources
         public string Version { get; } = version;
         public string Eula { get; } = eula;
         public string ConfigAnalyticsStatus { get; } = configAnalyticsStatus;
-        public ConfigAnalyticsCollectors AnalyticsCollectors { get; } = analyticsCollectors;
+        public Dictionary<string, ConfigValue> AnalyticsCollectors { get; } = analyticsCollectors;
         public string[][] BecomeMethods { get; } = becomeMethods;
         public bool UiNext { get; } = uiNext;
         public string ProjectBaseDir { get; } = projectBaseDir;
@@ -36,9 +36,6 @@ namespace AWX.Resources
                 sb.Append('}');
                 return sb.ToString();
             }
-        }
-        public class ConfigAnalyticsCollectors : Dictionary<string, ConfigValue>
-        {
         }
         public record ConfigValue(string Name, string Version, string Description)
         {
