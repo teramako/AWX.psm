226211b3bd185711effd928d1999d63a67264732
teramako <teramako@gmail.com>
2024-08-08 14:42:49 +0900

    Add summaries to class and properties

--- src/Resources/Config.cs
+++ src/Resources/Config.cs
@@ -1,7 +1,10 @@
-using System.Text.Json.Serialization;
+using System.Text;
 
 namespace AWX.Resources
 {
+    /// <summary>
+    /// Resource for <c>/api/v2/config/</c> (GET)
+    /// </summary>
     public class Config(string timeZone,
                         Config.ConfigLicenseInfo licenseInfo,
                         string version,
@@ -14,18 +17,42 @@ namespace AWX.Resources
                         string[] projectLocalPaths,
                         string[] customVirtualenvs)
     {
+        /// <summary>
+        /// The configured time zone for the server.
+        /// </summary>
         public string TimeZone { get; } = timeZone;
+        /// <summary>
+        /// Information about the current license.
+        /// </summary>
         public ConfigLicenseInfo LicenseInfo { get; } = licenseInfo;
+        /// <summary>
+        /// Version of Ansible Tower package installed.
+        /// </summary>
         public string Version { get; } = version;
+        /// <summary>
+        /// The current End-User License Agreement
+        /// </summary>
         public string Eula { get; } = eula;
         public string ConfigAnalyticsStatus { get; } = configAnalyticsStatus;
         public Dictionary<string, ConfigValue> AnalyticsCollectors { get; } = analyticsCollectors;
         public string[][] BecomeMethods { get; } = becomeMethods;
         public bool UiNext { get; } = uiNext;
+        /// <summary>
+        /// Path on the server where projects and playbooks are stored.
+        /// </summary>
         public string ProjectBaseDir { get; } = projectBaseDir;
+        /// <summary>
+        /// List of directories beneath <c>project_base_dir</c> to use when creating/editing a manual project.
+        /// </summary>
         public string[] ProjectLocalPaths { get; } = projectLocalPaths;
+        /// <summary>
+        /// Deprecated venv locations from before migration to execution environments. Export tooling is in <c>awx-manage</c> commands.
+        /// </summary>
         public string[] CustomVirtualenvs { get; } = customVirtualenvs;
 
+        /// <summary>
+        /// Information about the current license.
+        /// </summary>
         public record ConfigLicenseInfo(string LicenseType, bool ValidKey, string SubscriptionName, string ProductName)
         {
             public override string ToString()
@@ -37,6 +64,10 @@ namespace AWX.Resources
                 return sb.ToString();
             }
         }
+
+        /// <summary>
+        /// Value of <see cref="Config.AnalyticsCollectors"/>
+        /// </summary>
         public record ConfigValue(string Name, string Version, string Description)
         {
             public override string ToString()
