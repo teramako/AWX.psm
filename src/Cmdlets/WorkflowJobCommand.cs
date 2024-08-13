7988208b08d943eae0a49390029c76b9969428d1
teramako <teramako@gmail.com>
2024-08-13 12:37:08 +0900

    [psm] Update `Find-WorkflowJob`. Add `-Name`, `-Status`, `-LaunchType`

--- src/Cmdlets/WorkflowJobCommand.cs
+++ src/Cmdlets/WorkflowJobCommand.cs
@@ -36,11 +36,34 @@ namespace AWX.Cmdlets
         [Parameter(Mandatory = true, ParameterSetName = "AssociatedWith", ValueFromPipelineByPropertyName = true)]
         public override ulong Id { get; set; }
 
+        [Parameter(Position = 0)]
+        public string[]? Name { get; set; }
+
+        [Parameter()]
+        [ValidateSet(typeof(EnumValidateSetGenerator<JobStatus>))]
+        public string[]? Status { get; set; }
+
+        [Parameter()]
+        [ValidateSet(typeof(EnumValidateSetGenerator<JobLaunchType>))]
+        public string[]? LaunchType { get; set; }
+
         [Parameter()]
         public override string[] OrderBy { get; set; } = ["!id"];
 
         protected override void BeginProcessing()
         {
+            if (Name != null)
+            {
+                Query.Add("name__in", string.Join(',', Name));
+            }
+            if (Status != null)
+            {
+                Query.Add("status__in", string.Join(',', Status));
+            }
+            if (LaunchType != null)
+            {
+                Query.Add("launch_type__in", string.Join(',', LaunchType));
+            }
             SetupCommonQuery();
         }
         protected override void ProcessRecord()
