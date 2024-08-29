7fd18d1c21e070d5c5e1876a2beada27bad34fe4
teramako <teramako@gmail.com>
2024-08-29 11:54:54 +0900

    :bug: fix `Get-ActivityStreamCommand`

--- src/Cmdlets/ActivityStreamCommand.cs
+++ src/Cmdlets/ActivityStreamCommand.cs
@@ -20,11 +20,19 @@ namespace AWX.Cmdlets
         }
         protected override void EndProcessing()
         {
-            Query.Add("id__in", string.Join(',', IdSet));
-            Query.Add("page_size", $"{IdSet.Count}");
-            foreach (var resultSet in GetResultSet<ActivityStream>($"{ActivityStream.PATH}?{Query}", true))
+            if (IdSet.Count == 1)
             {
-                WriteObject(resultSet.Results, true);
+                var res = GetResource<ActivityStream>($"{ActivityStream.PATH}{IdSet.First()}/");
+                WriteObject(res);
+            }
+            else
+            {
+                Query.Add("id__in", string.Join(',', IdSet));
+                Query.Add("page_size", $"{IdSet.Count}");
+                foreach (var resultSet in GetResultSet<ActivityStream>(ActivityStream.PATH, Query, true))
+                {
+                    WriteObject(resultSet.Results, true);
+                }
             }
         }
     }
