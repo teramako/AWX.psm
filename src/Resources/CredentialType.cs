e73615a209ab15d280acb8a4611ba1d6854ed600
teramako <teramako@gmail.com>
2024-08-30 12:57:57 +0900

    Fix `Input` and `Injectors` properties type for CredentialType (#26)
    In order to destroy `OrderedDictionary`.
    
    * :sparkles: Improve CredentialType's `Input` property type
      * Implement `CredentialTypeInputs` class
      * Implement `CredentialInputField` class
        * Implement `CredentialBoolInputField` and `CredentialStringInputField`
      * Implement `CredentialInputFieldConverter` class for deserializing to `CredentialInputField`
    * :sparkles: Improve CredentialType's `Injectors` property type
      * change to `Dictionary<string, Dictionary<string, string>>` instead of `OrderedDictionary`

--- src/Resources/CredentialType.cs
+++ src/Resources/CredentialType.cs
@@ -1,4 +1,6 @@
 using System.Collections.Specialized;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace AWX.Resources
@@ -67,8 +69,8 @@ namespace AWX.Resources
         /// </summary>
         string Description { get; }
         CredentialTypeKind Kind { get; }
-        OrderedDictionary Inputs { get; }
-        OrderedDictionary Injectors { get; }
+        CredentialTypeInputs Inputs { get; }
+        Dictionary<string, Dictionary<string, string>> Injectors { get; }
     }
 
     public class CredentialType(ulong id,
@@ -83,8 +85,8 @@ namespace AWX.Resources
                                 CredentialTypeKind kind,
                                 string nameSpace,
                                 bool managed,
-                                OrderedDictionary inputs,
-                                OrderedDictionary injectors)
+                                CredentialTypeInputs inputs,
+                                Dictionary<string, Dictionary<string, string>> injectors)
         : ICredentialType, IResource<CredentialType.Summary>
     {
         public const string PATH = "/api/v2/credential_types/";
@@ -133,8 +135,197 @@ namespace AWX.Resources
         public CredentialTypeKind Kind { get; } = kind;
         public string Namespace { get; } = nameSpace;
         public bool Managed { get; } = managed;
-        public OrderedDictionary Inputs { get; } = inputs;
-        public OrderedDictionary Injectors { get; } = injectors;
+        public CredentialTypeInputs Inputs { get; } = inputs;
+        public Dictionary<string, Dictionary<string, string>> Injectors { get; } = injectors;
+    }
+
+    public record CredentialTypeInputs(CredentialInputField[] Fields, string[]? Required)
+    {
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("{ ");
+            sb.Append("Fields = [");
+            if (Fields.Length > 0) sb.Append($" {string.Join(", ", Fields.Select(field => field.Id))} ");
+            sb.Append(']');
+            if (Required != null)
+            {
+                sb.Append(", Required = [");
+                if (Required.Length > 0) sb.Append($" {string.Join(", ", Required)} ");
+                sb.Append(']');
+            }
+            sb.Append(" }");
+            return sb.ToString();
+        }
+    }
+
+    [JsonConverter(typeof(CredentialInputFieldConverter))]
+    public abstract record CredentialInputField(string Id, string Label, string Type, string? HelpText);
+
+    public record CredentialBoolInputField(string Id,
+                                           string Label,
+                                           string Type,
+                                           string? HelpText,
+                                           bool? Default)
+        : CredentialInputField(Id, Label, Type, HelpText)
+    {
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append('{');
+            sb.Append($" Id = {Id}");
+            sb.Append($", Label = {Label}");
+            sb.Append($", Type = {Type}");
+            if (Default != null) sb.Append($", Default = {Default}");
+            if (HelpText != null) sb.Append($", HelpText = {HelpText}");
+            sb.Append(" }");
+            return sb.ToString();
+        }
+    }
+    public record CredentialStringInputField(string Id,
+                                             string Label,
+                                             string Type,
+                                             string? HelpText,
+                                             string[]? Choices,
+                                             string? Format,
+                                             bool Secret = false,
+                                             bool Multiline = false,
+                                             string? Default = null)
+        : CredentialInputField(Id, Label, Type, HelpText)
+    {
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append('{');
+            sb.Append($" Id = {Id}");
+            sb.Append($", Label = {Label}");
+            sb.Append($", Type = {Type}");
+            if (Default != null) sb.Append($", Default = {Default}");
+            if (Choices != null) sb.Append($", Choices = [{string.Join(", ", Choices)}]");
+            if (Format != null) sb.Append($", Format = {Format}");
+            if (Secret) sb.Append(", Secret = True");
+            if (Multiline) sb.Append(", Multiline = True");
+            if (HelpText != null) sb.Append($", HelpText = {HelpText}");
+            sb.Append(" }");
+            return sb.ToString();
+        }
+    }
+
+    class CredentialInputFieldConverter : JsonConverter<CredentialInputField>
+    {
+        public override CredentialInputField? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            (string id, string label, string? help_text, string[]? choices, string? format, string type, bool secret, bool multiline) =
+                ("", "", null, null, null, "string", false, false);
+            string? defaultString = null;
+            bool? defaultBool = null;
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                {
+                    switch (type)
+                    {
+                        case "boolean":
+                            return new CredentialBoolInputField(id, label, type, help_text, defaultBool);
+                        case "string":
+                        default:
+                            return new CredentialStringInputField(
+                                    id, label, type, help_text, choices, format, secret, multiline, defaultString);
+                    }
+                }
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new JsonException($"TokenType is not PropertyName: {reader.TokenType}");
+                }
+                string propertyName = reader.GetString() ?? throw new JsonException("PropertyName is null");
+                reader.Read();
+                switch (propertyName)
+                {
+                    case "id":
+                        id = reader.GetString() ?? "";
+                        break;
+                    case "label":
+                        label = reader.GetString() ?? "";
+                        break;
+                    case "help_text":
+                        help_text = reader.GetString();
+                        break;
+                    case "choices":
+                        choices = JsonSerializer.Deserialize<string[]>(ref reader, options);
+                        break;
+                    case "format":
+                        format = reader.GetString();
+                        break;
+                    case "secret":
+                        secret = reader.GetBoolean();
+                        break;
+                    case "multiline":
+                        multiline = reader.GetBoolean();
+                        break;
+                    case "type":
+                        type = reader.GetString() ?? "string";
+                        break;
+                    case "default":
+                        switch (reader.TokenType)
+                        {
+                            case JsonTokenType.String:
+                                defaultString = reader.GetString() ?? "";
+                                break;
+                            case JsonTokenType.True:
+                            case JsonTokenType.False:
+                                defaultBool = reader.GetBoolean();
+                                break;
+                        }
+                        break;
+                }
+            }
+            throw new JsonException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, CredentialInputField value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteString("id", value.Id);
+            writer.WriteString("label", value.Label);
+            writer.WriteString("type", value.Type);
+            if (value.HelpText != null)
+            {
+                writer.WriteString("help_text", value.HelpText);
+            }
+            switch (value)
+            {
+                case CredentialBoolInputField boolField:
+                    if (boolField.Default != null)
+                    {
+                        writer.WriteBoolean("default", (bool)boolField.Default);
+                    }
+                    break;
+                case CredentialStringInputField strField:
+                    if (strField.Choices != null)
+                    {
+                        writer.WritePropertyName("choices");
+                        JsonSerializer.Serialize(writer, strField.Choices, options);
+                    }
+                    if (strField.Default != null)
+                    {
+                        writer.WriteString("default", strField.Default);
+                    }
+                    if (strField.Format != null)
+                    {
+                        writer.WriteString("format", strField.Format);
+                    }
+                    if (strField.Secret)
+                    {
+                        writer.WriteBoolean("secret", strField.Secret);
+                    }
+                    if (strField.Multiline)
+                    {
+                        writer.WriteBoolean("multiline", strField.Multiline);
+                    }
+                    break;
+            }
+            writer.WriteEndObject();
+        }
     }
 }
 
