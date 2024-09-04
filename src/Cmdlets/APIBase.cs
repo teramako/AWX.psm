35b75dc1b6e08d22d3e0eb06e7b4bd29ed0a97ce
teramako <teramako@gmail.com>
2024-09-04 18:11:30 +0900

    :sparkles: Implement `AskSurvey()` to `InvokeJobBase` class

--- src/Cmdlets/APIBase.cs
+++ src/Cmdlets/APIBase.cs
@@ -4,6 +4,7 @@ using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Management.Automation;
 using System.Text;
+using System.Text.Json;
 using System.Web;
 
 namespace AWX.Cmdlets
@@ -118,6 +119,113 @@ namespace AWX.Cmdlets
             }
             WriteHost(sb.ToString());
             ui.WriteLine("\n");
+        }
+
+        /// <summary>
+        /// Get SurveySpecs and show input prompts.
+        /// </summary>
+        /// <param name="type">Resource Type. Should be either <see cref="ResourceType.JobTemplate"/> or <see cref="ResourceType.WorkflowJobTemplate"/></param>
+        /// <param name="id">ID number of either JobTemplate or WorkflowJobTemplate</param>
+        /// <param name="onlyRequired">Show prompts for only required.</param>
+        /// <param name="sendData">Results of input prompts will be stored into <c>extra_vars</c> key's property in this dictionary.</param>
+        /// <returns>Whether the prompt is inputed(<c>true</c>) or Canceled(<c>false</c>)</returns>
+        protected bool AskSurvey(ResourceType type, ulong id, bool onlyRequired, IDictionary<string, object?> sendData)
+        {
+            var surveyPath = type switch
+            {
+                ResourceType.JobTemplate => $"{JobTemplate.PATH}{id}/survey_spec/",
+                ResourceType.WorkflowJobTemplate => $"{WorkflowJobTemplate.PATH}{id}/survey_spec/",
+                _ => throw new ArgumentException($"Invalid Resource Type: {type}")
+            };
+
+            var survey = GetResource<Survey>(surveyPath);
+            var extraVars = sendData.ContainsKey("extra_vars")
+                ? Yaml.DeserializeToDict(sendData["extra_vars"] as string ?? "")
+                : new Dictionary<string, object?>();
+            if (survey != null && survey.Spec?.Length > 0)
+            {
+                if (CommandRuntime.Host == null)
+                    return false;
+
+                var prompt = new AskPrompt(CommandRuntime.Host);
+
+                foreach (var spec in survey.Spec)
+                {
+                    var varName = spec.Variable;
+                    if (extraVars.ContainsKey(varName))
+                    {
+                        WriteHost($"Skip Survey[{varName}] prompt. Already specified: {JsonSerializer.Serialize(extraVars[varName])}",
+                                  dontshow: true);
+                        continue;
+                    }
+                    if (onlyRequired && !spec.Required)
+                        continue;
+
+                    var label = $"Survey {spec.QuestionName}";
+                    var key = $"extra_vars.{varName}";
+                    var description = $"Variable: [{varName}]"
+                        + (string.IsNullOrEmpty(spec.QuestionDescription) ? "" : $", Description: {spec.QuestionDescription}");
+                    switch (spec.Type)
+                    {
+                        case SurveySpecType.Text:
+                        case SurveySpecType.Textarea: // FIXME
+                            if (prompt.Ask(label, key, spec.Default as string, description, out var stringAnswer))
+                            {
+                                extraVars[varName] = stringAnswer.Input;
+                                PrintPromptResult(varName, $"\"{stringAnswer.Input}\"", stringAnswer.IsEmpty);
+                                continue;
+                            }
+                            return false;
+                        case SurveySpecType.Integer:
+                            int? intDefault = string.IsNullOrEmpty(spec.Default as string) ? null : (int)spec.Default;
+                            if (prompt.Ask<int>(label, key, (int?)spec.Default, description, spec.Required, out var intAnswer))
+                            {
+                                extraVars[varName] = intAnswer.Input;
+                                PrintPromptResult(varName, $"{intAnswer.Input}", intAnswer.IsEmpty);
+                                continue;
+                            }
+                            return false;
+                        case SurveySpecType.Float:
+                            float? floatDefault = string.IsNullOrEmpty(spec.Default as string) ? null : (float)spec.Default;
+                            if (prompt.Ask<float>(label, key, floatDefault, description, spec.Required, out var floatAnswer))
+                            {
+                                extraVars[varName] = floatAnswer.Input;
+                                PrintPromptResult(varName, $"{floatAnswer.Input}", floatAnswer.IsEmpty);
+                                continue;
+                            }
+                            return false;
+                        case SurveySpecType.MultipleChoice:
+                            var choiceFields = (spec.Choices as string[] ?? []).Select(val => (val, val)).ToArray();
+                            if (prompt.AskSelectOne(label, choiceFields, spec.Default as string ?? "", description, out var oneAnswer))
+                            {
+                                extraVars[varName] = oneAnswer.Input;
+                                PrintPromptResult(varName, $"\"{oneAnswer.Input}\"", oneAnswer.IsEmpty);
+                                continue;
+                            }
+                            return false;
+                        case SurveySpecType.MultiSelect:
+                            var multiFields = (spec.Choices as string[] ?? []).Select(val => (val, val)).ToArray();
+                            var defaultValues = (spec.Default as string ?? "").Split('\n');
+                            if (prompt.AskSelectMulti(label, key, multiFields, defaultValues, description, out var multiAnswer))
+                            {
+                                extraVars[varName] = multiAnswer.Input;
+                                PrintPromptResult(varName, $"[{string.Join(", ", multiAnswer.Input.Select(x => $"\"{x}\""))}]", multiAnswer.IsEmpty);
+                            continue;
+                            }
+                            return false;
+                        case SurveySpecType.Password:
+                            if (prompt.AskPassword(label, key, description, out var passwordAnswer))
+                            {
+                                extraVars[varName] = passwordAnswer.Input;
+                                PrintPromptResult(varName, string.Empty);
+                                continue;
+                            }
+                            return false;
+                    }
+                }
+                sendData["extra_vars"] = JsonSerializer.Serialize(extraVars);
+            }
+            return true;
         }
     }
 
